4dd26f585e7d010de03d48be6d396ebde8da2b95
Varma-81 <nithinpathuri95@gmail.com>
2026-03-27 17:13:29 +0530

    Added abstract calculator structure with basic class setup

---
+++ src/BasicCalculator.java
@@ -0,0 +1,17 @@
+public class BasicCalculator extends Calculator {
+
+    @Override
+    public int addition(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int subtraction(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int multiplication(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+public abstract class Calculator {
+
+    public abstract int addition(int a, int b);
+
+    public abstract int subtraction(int a, int b);
+
+    public abstract int multiplication(int a, int b);
+}
