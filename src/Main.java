2b0bb5ba7339200c5c1d14f110966177f702a22d
Varma-81 <nithinpathuri95@gmail.com>
2026-03-27 17:18:14 +0530

    Implemented calculator methods for addition subtraction and multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calculator project created.");
+        
+        BasicCalculator calculator = new BasicCalculator();
+
+        System.out.println("Addition: " + calculator.addition(10, 5));
+        System.out.println("Subtraction: " + calculator.subtraction(10, 5));
+        System.out.println("Multiplication: " + calculator.multiplication(10, 5));
+
     }
 }
